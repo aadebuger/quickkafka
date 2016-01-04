9989257b38438dc11a450f21e4035e6e615639dc
aadebuger <zhvxxh@gmail.com>
2016-01-04 18:03:40 +0800

    new sed

--- runkafkaserver.sh
+++ runkafkaserver.sh
@@ -1,8 +1,7 @@
 docker stop quickkafkaserver || echo "stop quickkafkaserver"
 docker rm quickkafkaserver || echo "rm quickkafkaserver" 
 dockerip=`docker inspect quickkafkazookeeper |jq -r .[0].NetworkSettings.IPAddress`
-
-sed "s/172.17.9.103/$dockerip/g"  config/server.tmpl > config/server1.tmpl
+#sed "s/172.17.9.103/$dockerip/g"  config/server.tmpl > config/server1.tmpl
  
-sed "s/1257.net/$hostip/g"  config/server1.tmpl > config/server.properties
-docker run -d --name quickkafkaserver -p 9092:9092 -v $PWD/kafka-logs:/tmp/kafka-logs -v $PWD/config:/opt/kafka_2.11-0.9.0.0/config   aadebuger/quickkafka-kafkaserver 
+#sed "s/1257.net/$hostip/g"  config/server1.tmpl > config/server.properties
+docker run -d --name quickkafkaserver -p 9092:9092 -e dockerip=$dockerip -e hostip=1257.net -v $PWD/kafka-logs:/tmp/kafka-logs -v $PWD/config:/opt/kafka_2.11-0.9.0.0/config1   aadebuger/quickkafka-kafkaserver 
